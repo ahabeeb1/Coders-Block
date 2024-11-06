5701e1e8507c1a10b01e6b1b67f7b88ddef4e731
ahabeeb1 <34636534+ahabeeb1@users.noreply.github.com>
2024-11-06 12:37:36 -0500

    Git init for Coders-Block

---
+++ coders-block-application/src/main/java/com/codersblock/coders_block_application/CodersBlockApplication.java
@@ -0,0 +1,13 @@
+package com.codersblock.coders_block_application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodersBlockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodersBlockApplication.class, args);
+	}
+
+}
